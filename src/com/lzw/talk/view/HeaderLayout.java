041ac94fd4bbb29179ee78cbe23f302533d826a2
lzwjava <lzwjava@gmail.com>
2014-09-17 17:07:55 +0800

    finish mainActivity

--- src/com/lzw/talk/view/HeaderLayout.java
+++ src/com/lzw/talk/view/HeaderLayout.java
@@ -1,8 +1,11 @@
 package com.lzw.talk.view;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -16,6 +19,7 @@ public class HeaderLayout extends LinearLayout {
   RelativeLayout header;
   TextView titleView;
   LinearLayout leftContainer, rightContainer;
+  Button backBtn;
 
   public HeaderLayout(Context context) {
     super(context);
@@ -33,13 +37,26 @@ public class HeaderLayout extends LinearLayout {
     titleView = (TextView) header.findViewById(R.id.titleView);
     leftContainer = (LinearLayout) header.findViewById(R.id.leftContainer);
     rightContainer = (LinearLayout) header.findViewById(R.id.rightContainer);
+    backBtn = (Button) header.findViewById(R.id.backBtn);
     addView(header);
   }
 
-  public void setTitle(int titleId){
+  public void setTitle(int titleId) {
     titleView.setText(titleId);
   }
 
-  public void showLeftBackButton(int backTextId){
+  public void showLeftBackButton() {
+    showLeftBackButton(R.string.emptyStr);
+  }
+
+  public void showLeftBackButton(int backTextId) {
+    backBtn.setVisibility(View.VISIBLE);
+    backBtn.setText(backTextId);
+    backBtn.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        ((Activity) getContext()).finish();
+      }
+    });
   }
 }
---
+++ src/com/lzw/talk/view/fragment/DiscoverFragment.java
@@ -0,0 +1,23 @@
+package com.lzw.talk.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.lzw.talk.R;
+
+/**
+ * Created by lzw on 14-9-17.
+ */
+public class DiscoverFragment extends BaseFragment {
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.discover_fragment, null);
+  }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    headerLayout.setTitle(R.string.discover);
+  }
+}
