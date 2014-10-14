9ff8f9edacecdee522d7add1957dd458a8b07c81
lzwjava <lzwjava@gmail.com>
2014-10-14 10:50:31 +0800

    add a line

--- src/com/lzw/talk/ui/activity/ChatActivity.java
+++ src/com/lzw/talk/ui/activity/ChatActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
