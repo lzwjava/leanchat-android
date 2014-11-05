47e1a07993f0f3c0e538e00ee94e83f28c0fbb02
lzwjava <lzwjava@gmail.com>
2014-11-05 12:51:43 +0800

    clean code

--- src/com/avoscloud/chat/util/PixelUtil.java
+++ src/com/avoscloud/chat/util/PixelUtil.java
@@ -1,8 +1,6 @@
 package com.avoscloud.chat.util;
 
-import android.content.Context;
 import android.content.res.Resources;
-import com.avoscloud.chat.base.App;
 
 /**
  * 像素转换工具
@@ -10,114 +8,27 @@ import com.avoscloud.chat.base.App;
  * @author MarkMjw
  */
 public class PixelUtil {
+  private static Resources resources = Resources.getSystem();
+  private static int desityDpi = resources.getDisplayMetrics().densityDpi;
+  private static float scaledDensity = resources.getDisplayMetrics().scaledDensity;
 
-  /**
-   * The context.
-   */
-  private static Context mContext = App.ctx;
-
-  /**
-   * dp转 px.
-   *
-   * @param value the value
-   * @return the int
-   */
   public static int dp2px(float value) {
-    final float scale = mContext.getResources().getDisplayMetrics().densityDpi;
+    final float scale = desityDpi;
     return (int) (value * (scale / 160) + 0.5f);
   }
 
-  /**
-   * dp转 px.
-   *
-   * @param value   the value
-   * @param context the context
-   * @return the int
-   */
-  public static int dp2px(float value, Context context) {
-    final float scale = context.getResources().getDisplayMetrics().densityDpi;
-    return (int) (value * (scale / 160) + 0.5f);
-  }
-
-  /**
-   * px转dp.
-   *
-   * @param value the value
-   * @return the int
-   */
   public static int px2dp(float value) {
-    final float scale = mContext.getResources().getDisplayMetrics().densityDpi;
+    final float scale = desityDpi;
     return (int) ((value * 160) / scale + 0.5f);
   }
 
-  /**
-   * px转dp.
-   *
-   * @param value   the value
-   * @param context the context
-   * @return the int
-   */
-  public static int px2dp(float value, Context context) {
-    final float scale = context.getResources().getDisplayMetrics().densityDpi;
-    return (int) ((value * 160) / scale + 0.5f);
-  }
-
-  /**
-   * sp转px.
-   *
-   * @param value the value
-   * @return the int
-   */
   public static int sp2px(float value) {
-    Resources r;
-    if (mContext == null) {
-      r = Resources.getSystem();
-    } else {
-      r = mContext.getResources();
-    }
-    float spvalue = value * r.getDisplayMetrics().scaledDensity;
+    float spvalue = value * scaledDensity;
     return (int) (spvalue + 0.5f);
   }
 
-  /**
-   * sp转px.
-   *
-   * @param value   the value
-   * @param context the context
-   * @return the int
-   */
-  public static int sp2px(float value, Context context) {
-    Resources r;
-    if (context == null) {
-      r = Resources.getSystem();
-    } else {
-      r = context.getResources();
-    }
-    float spvalue = value * r.getDisplayMetrics().scaledDensity;
-    return (int) (spvalue + 0.5f);
-  }
-
-  /**
-   * px转sp.
-   *
-   * @param value the value
-   * @return the int
-   */
   public static int px2sp(float value) {
-    final float scale = mContext.getResources().getDisplayMetrics().scaledDensity;
+    final float scale = scaledDensity;
     return (int) (value / scale + 0.5f);
   }
-
-  /**
-   * px转sp.
-   *
-   * @param value   the value
-   * @param context the context
-   * @return the int
-   */
-  public static int px2sp(float value, Context context) {
-    final float scale = context.getResources().getDisplayMetrics().scaledDensity;
-    return (int) (value / scale + 0.5f);
-  }
-
 }
